a00c6555c4e2920a702bedb069c27e7f610e7a5d
hezhengbao <958493843@qq.com>
2020-12-23 16:50:27 +0800

    tmp

--- static/js/my.js
+++ static/js/my.js
@@ -44,19 +44,62 @@ document.querySelector("#upload-file").onchange = () => {
 
 
 // ------------------------------------加载图表----------------------------------------------
-var chart = echarts.init(document.getElementById('bar'), 'dark', {renderer: 'canvas'});
+var chart = echarts.init(document.getElementById('chart'), 'dark', {renderer: 'canvas'});
 chart.showLoading();
 const myRequest = new Request("http://127.0.0.1:9584/data");
 
-var alldata;
+var allData = null;
 
 
 fetch(myRequest)
     // .then(chart.hideLoading();)
     .then(re => re.json())// 获取json文件
-    .then((data) => {alldata=data.yawing0_bad; return data.yawing0_bad;})
-    .then(data => get_opt(data))
-    .then(opt => {chart.hideLoading();chart.setOption(opt);});
+    .then(process_add_button) // 处理数据 添加按钮
+    .then(data => get_opt(data)) // 获取opt
+    .then(opt => {chart.hideLoading();chart.setOption(opt);}) // 绘图
+    .then(add_event) // 为每一个button添加触发事件
+
+
+// 处理返回的json数据，添加Yawing按钮
+function process_add_button(data){
+    let yawing = document.getElementById('yawing');
+    if (yawing !== null){yawing.parentNode.removeChild(yawing);}
+    yawing = document.createElement("div");
+    yawing.id = 'yawing';
+    let count = 0;
+    for (let ele in data) {
+        button = document.createElement("button");
+        button.class = 'y';
+        button.innerText = 'Yawing'+String(count);
+        if ( Object.keys(data)[count].indexOf('bad') !== -1){
+            button.value = 'bad';
+        } else {
+            button.value = 'good';
+        }
+        count++;
+        yawing.appendChild(button);
+    };
+    document.getElementById('pic').insertBefore(yawing,document.getElementById('chart'));
+    allData = data;
+    return data[Object.keys(data)[0]];
+}
+
+function flash_chart(e) {
+    console.log("Clicking here：",e.target)
+    index = Number(e.target.innerText[e.target.innerText.length-1]);
+    chart.setOption(get_opt(allData[Object.keys(allData)[index]]));
+}
+
+
+function add_event(){ 
+    console.log('add event')
+    buttons = document.querySelectorAll('#yawing button');
+    for (i=0;i< buttons.length;i++){
+        everyButton = buttons[i]
+        console.log("adding on",everyButton.innerText)
+        everyButton.onclick = flash_chart;
+    }
+}
 
 
 // 指定图表的配置项和数据
@@ -78,7 +121,7 @@ function get_opt(metadata) {
         tooltip: {
             trigger: 'axis',
             axisPointer : {            // 坐标轴指示器，坐标轴触发有效
-                type : 'shadow'        // 默认为直线，可选为：'line' | 'shadow'
+                type : 'cross'        // 默认为直线，可选为：'line' | 'shadow'
             }
         },
 
@@ -89,7 +132,6 @@ function get_opt(metadata) {
                     yAxisIndex: 'none'
                 },
                 dataView: {readOnly: false},
-                magicType: {type: ['line', 'bar']},
                 restore: {},
                 saveAsImage: {}
             }
@@ -116,12 +158,13 @@ function get_opt(metadata) {
 
         dataZoom: [{
             type: 'inside',
-            xAxisIndex: [0, 1],
+            xAxisIndex: [0,1],
             start: 0,
-            end: 10
+            end: 100
         }, {
             start: 0,
-            end: 10,
+            end: 100,
+            xAxisIndex: [0,1],
             handleIcon: 'M10.7,11.9v-1.3H9.3v1.3c-4.9,0.3-8.8,4.4-8.8,9.4c0,5,3.9,9.1,8.8,9.4v1.3h1.3v-1.3c4.9-0.3,8.8-4.4,8.8-9.4C19.5,16.3,15.6,12.2,10.7,11.9z M13.3,24.4H6.7V23h6.6V24.4z M13.3,19.6H6.7v-1.4h6.6V19.6z',
             handleSize: '80%',
             handleStyle: {
@@ -151,10 +194,10 @@ function get_opt(metadata) {
             boundaryGap: false,
             data: metadata.Timestamp,
             gridIndex: 0,
-        }, {
+        }, {  // 几张图，几个xAxis，yAxis
             type: 'category',
             boundaryGap: false,
-            data: metadata.TimeStamp,
+            data: metadata.Timestamp,
             gridIndex: 1,
         }],
 
